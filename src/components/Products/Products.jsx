956cb5f05d2f00e861c37a82279a949eff38ece2
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-09-18 00:02:23 +0100

    updates on Products.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import Product from './Product/Product';
 
 const products = [
     { id: 1, name: "Laptop", category: "Black Apple Macbook"},
