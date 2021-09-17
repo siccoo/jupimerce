a4dce6463b837101a7be94536f71508083c9b531
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-09-17 12:59:58 +0100

    updated Products.jsx by pasing some props of id, name, category

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+const products = [
+    { id: 1, name: "Laptop", category: "Black Apple Macbook"},
+    { id: 2, name: "Table", category: "Blue Table"},
+    { id: 3, name: "Clothes", category: "Various Tops and Sleeves"},
+    { id: 4, name: "Electronics", category: "All kinds of TV's"},
+]
+
+const Products = () => {
+    <main>
+        <Grid container justify="center" spacing={4}>
+            {products.map((product) => (
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product />
+                </Grid>
+            ))}
+        </Grid>
+    </main>
+}
+
+export default Products
