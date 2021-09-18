121d47a6c5ec33e7f96e176c80f65a965ce13720
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-09-18 13:12:30 +0100

    updates on Product.jsx

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from "@material-ui/core";
-import { AddCircleOutline } from '@material-ui/icons';
+import { AddCircleOutlineIcon } from '@material-ui/icons';
 
 const Product = ({ product }) => {
     return (
@@ -15,7 +15,13 @@ const Product = ({ product }) => {
                         {product.price}
                     </Typography>
                 </div>
+                <Typography variant="h2" color="textSecondary">{product.description}</Typography>
             </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label="Add to Cart">
+                    <AddCircleOutline />
+                </IconButton>
+            </CardActions>
         </Card>
     )
 }
