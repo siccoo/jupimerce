53232339ead05e23aba2934a18653f7a0673e474
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-09-18 13:53:25 +0100

    updated Product.jsx, App.js, styles.js

---
+++ src/components/Products/Product/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles({
+    root: {
+        maxWidth: "100%",
+    },
+    media: {
+        height: 0,
+        paddingTop: "56.29%",
+    },
+    content: {
+        display: flex,
+        justifyContent: "flex-end",
+    },
+    cardActions: {
+        display: flex,
+        justifyContent: "space-between"
+    }
+});
